8f5ba02c5126329acce7815890072e5797ef64c5
Wiliam-L <wlopezguerra2000@gmail.com>
2024-10-16 16:27:42 -0600

    add config database - postgresql

--- changeBook_api/settings.py
+++ changeBook_api/settings.py
@@ -93,10 +93,14 @@ SIMPLE_JWT = {
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': config('NAME'),
+    'USER': config('USER'),
+    'PASSWORD': config('PASSWORD'),
+    'HOST': config('HOST'),
+    'PORT': config('PORT'),
+  }
 }
 
 

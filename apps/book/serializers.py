4a258c5484a8710bb7fea369a2986a6dc14f2396
Wiliam-L <wlopezguerra2000@gmail.com>
2024-10-16 17:34:53 -0600

    refacto PostBookSerializer

--- apps/book/serializers.py
+++ apps/book/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from django.db.transaction import atomic
 from .models import BookStatus, CategoryBook, ExchangeType, PostBook, CustomUser
 from cloudinary.uploader import upload
+
 from  .api_cloudinary import api_cloudinary
 api_cloudinary()
 
@@ -12,22 +13,27 @@ class PostBookSerializer(serializers.ModelSerializer):
         fields = ['user', 'image_url', 'image_id', 'name', 'author', 'category', 'status', 'exhange_type']
         read_only_fields = ['creation_date', 'update_date']
 
-    def __init__(self, *args, **kwargs):
-        super(PostBookSerializer, self).__init__(*args, **kwargs)
+    def validateData(self, value, message):
+        if not value: 
+           raise serializers.ValidationError({'message': message})
+        return
 
-        #si es un put entonces se acepta campos en blanco
-        if self.instance:
-            for field in ['image_url', 'image_id', 'name', 'author', 'category', 'status', 'exhange_type']:
-                self.fields[field].required = False
-                self.fields[field].allow_blank = True
-    
     def validate(self, data):
         instance = self.instance
-        image = data.get('image')
+        image = data.get('image', instance.image_url if instance else None)
+        name_book = data.get('name', instance.name if instance else None)
+        author = data.get('author', instance.author if instance else None)
+        category = data.get('category', instance.category if instance else None)
+        status = data.get('status', instance.status if instance else None)
+        exhange_type = data.get('exhange_type', instance.exhange_type if instance else None)
 
-        if not instance:
-            if not image: raise serializers.ValidationError({"message": "imagen obligatorio"})
-        
+        self.validateData(image, 'La imagen obligatoria')
+        self.validateData(name_book, 'El nombre del libro obligatorio')
+        self.validateData(author, 'El author del libro obligatorio')
+        self.validateData(category, 'La categoria para el libro obligatorio')
+        self.validateData(status, 'El estado del libro es obligatorio')
+        self.validateData(exhange_type, 'El tipo de cambio de libros obligatorio')
+
         return data
 
     @atomic
@@ -35,11 +41,22 @@ class PostBookSerializer(serializers.ModelSerializer):
         image = validated_data.pop('image', None)
         request_user = self.context['request'].user
         validated_data['user'] = request_user
+
         if image:
             try:
+                #llamada para subir la imagen a Cloudinary
                 upload_result = self.upload_image_post(image)
-                validated_data['image_url'] = upload_result.get('url')
-                validated_data['image_id'] = upload_result.get('public_id')
+
+                #verificar si url y public_id fueren devueltos
+                image_url = upload_result.get('url')
+                image_id = upload_result.get('public_id')
+
+                if not image_url or not image_id:
+                    raise serializers.ValidationError('Error al obtener la URL o ID de la imagen.')
+
+                # Asignar image_url e image_id al validated_data
+                validated_data['image_url'] = image_url
+                validated_data['image_id'] = image_id
 
                 postBook = PostBook.objects.create(**validated_data)
                 postBook.save()
@@ -59,7 +76,6 @@ class PostBookSerializer(serializers.ModelSerializer):
             print(f"Error subiendo la imagen: {str(e)}")
 
             raise e
-
 
 class CategorySerializer(serializers.ModelSerializer):
     pass

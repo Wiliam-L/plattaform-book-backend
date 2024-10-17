3ca17929680258a1a5b88b34a214c20fb2511ad1
Wiliam-L <wlopezguerra2000@gmail.com>
2024-10-16 21:08:19 -0600

    add router - category and type

--- apps/administrator/urls.py
+++ apps/administrator/urls.py
@@ -1,9 +1,12 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from .views import PostBookViewSet
+from apps.book.views import ExchageTypeView, CategoryView
 
 router = DefaultRouter()
 router.register(r'post-book', PostBookViewSet, basename='post-book')
+router.register(r'category', CategoryView, basename='category')
+router.register(r'type', ExchageTypeView, basename='type')
 
 urlpatterns = [
     path('', include(router.urls)),
